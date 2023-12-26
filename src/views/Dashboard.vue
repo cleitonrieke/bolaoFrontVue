62e7036ce6f905c366bea38fe0c602930b35ec0b
Cleiton Rieke <cleitonrieke@gmail.com>
2023-12-26 20:03:59 -0300

    melhorias

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,12 +4,16 @@ import http from '../http-common.js'
 import Bolao from '../models/bolao.js'
 
 let participantes = ref([])
+let numeros = ref([])
 
 const initialize = (participantes) => {    
     console.log(participantes)
-    http.get('/boloes').then((res) => {
-        console.log(res.data)
+    http.get('/boloes').then((res) => {        
         participantes.value = res.data        
+    })
+    http.get('/numeros').then((res) => {      
+        console.log(res.data)  
+        numeros.value = res.data        
     })
 }
 
@@ -35,7 +39,23 @@ initialize(participantes)
             </table>            
         </div>
         <div class="row">
-
+            &nbsp;
+        </div>
+        <div class="row">
+            <table class="table table-striped">
+                <thead>
+                    <tr class="table-dark">                        
+                        <th>Número</th>
+                        <th>Quantidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in numeros">                        
+                        <td>{{ item.numero }}</td>
+                        <td>{{ item.qtd }}</td>
+                    </tr>
+                </tbody>
+            </table>            
         </div>
     
 </template>
